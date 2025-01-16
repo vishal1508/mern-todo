98ff1f91be7115b4ddb4bc8d1432bceded1b6f26
vishal1508 <vishalkerlekar15@gmail.com>
2025-01-16 22:59:51 +0530

    make the dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,31 @@
-import React from 'react'
-import { BrowserRouter, Routes,Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Dashboard from './pages/Dashboard'
-import About from './pages/About'
-import SignUp from './pages/SignUp'
-import SignIn from './pages/SignIn'
-import Project from './pages/Project'
-import Header from './components/Header'
-import FooterComp from './components/Footer'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Project from "./pages/Project";
+import Header from "./components/Header";
+import FooterComp from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 const App = () => {
   return (
     <BrowserRouter>
-      <Header/>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/dashboard' element={<Dashboard/>}/>
-        <Route path='/signin' element={<SignIn/>}/>
-        <Route path='/signup' element={<SignUp/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/projects' element={<Project/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Project />} />
       </Routes>
-      <FooterComp/>
+      <FooterComp />
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -5,23 +5,25 @@ import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
 //combine Reducer user CombineReducerFunction
-// const rootReducer = combineReducers({
-//   user:userReducer,
-//   theme:themeReducer,
-// })
+const rootReducer = combineReducers({
+  user:userReducer,
+  theme:themeReducer,
+})
 // Persist configuration
 const persistConfig = {
   key: "root", // Key to identify persisted state in storage
   storage, // Use localStorage for persisting
   version: 1,
-  whitelist: ["theme"], // Only persist theme slice
+  whitelist: ['user', 'theme'], // Only persist theme slice
 };
 // Create a persisted reducer
-const persistedUserReducer = persistReducer(persistConfig, userReducer);
-const persistedThemeReducer = persistReducer(persistConfig, themeReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const persistedUserReducer = persistReducer(persistConfig, userReducer);
+// const persistedThemeReducer = persistReducer(persistConfig, themeReducer);
 
 export const store = configureStore({
-  reducer:{user:persistedUserReducer,theme:persistedThemeReducer},
+  reducer:persistedReducer,
+  // reducer:{user:persistedUserReducer,theme:persistedThemeReducer},
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }),
 });
